76a131c7e89f42ef7836b6a62a55792d05b5f96f
Liudmila <milalindley@gmail.com>
2025-12-17 18:42:06 +0100

    feat: create collection of employees

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,46 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Stream;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        List<Employee> employees = readFile().stream()
+                .filter(e -> !e.isBlank())
+                .map(str -> {
+                    String[] arr = str.trim().split(" ");
+                    return new Employee(arr[0], arr[1]);
+                }).toList();
+
+        System.out.println("Size of collection: " + employees.size());
+
+        System.out.println("Print employees using foreach loop");
+        employees.forEach(System.out::println);
+
+        System.out.println("Print employees using standard loop");
+        for (Employee employee : employees) {
+            System.out.println(employee);
+        }
+
+        System.out.println("Print employees using Iterator interface");
+        for (Iterator<Employee> iterator = employees.iterator(); iterator.hasNext(); ) {
+            Employee next = iterator.next();
+            System.out.println(next);
+        }
 
     }
+
+    private static List<String> readFile() {
+        try (BufferedReader br = new BufferedReader(new FileReader("resources/employees.txt"))) {
+            return br.lines().toList();
+        } catch (IOException e) {
+            throw new RuntimeException("Error reading file.");
+        }
+    }
+
 }
